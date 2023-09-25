c62ac44eb90ae9ddf3b9dec994b400b785e28c17
allaberx <allaberdinx@gmail.com>
2023-09-25 12:52:57 +0500

    Добавил навигацию на главный экарн

--- app/src/main/java/com/allaber/test/presentation/view/MainActivity.kt
+++ app/src/main/java/com/allaber/test/presentation/view/MainActivity.kt
@@ -2,7 +2,6 @@ package com.allaber.test.presentation.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
 import com.allaber.test.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,7 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
-        navController.navigate(R.id.homeFragment)
     }
 }
